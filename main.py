6a9d867d12794ce96996406f77a82e2aa843456a
plugs <plugs2004@gmail.com>
2023-12-09 12:19:44 +0700

    Final

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ from model import predict as model_predict
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+from ultralytics import YOLO
 
 # create app
 app = FastAPI()
 # alow cross origin all origin
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # index route
 @app.get('/')
@@ -17,7 +25,13 @@ def index():
 # predict route
 @app.get('/predict')
 def predict(URL: str, MODEL: str):
-    return {'text': model_predict(URL, MODEL)}
+    if URL is None or MODEL is None:
+        return {'error': 'Both URL and MODEL parameters are required.'}
+    
+    pred = model_predict(URL, MODEL)
+    return {'Level': pred[1],
+            'Confidence': pred[0]
+            }
 
 # start server
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -1,15 +1,19 @@
 from ultralytics import YOLO
 # Run inference
-def predict(INPUT, MODEL):
-    if(MODEL == 'abrasion'):
-        results = abrasion_model(INPUT)
-    if(MODEL == 'burn'):
-        results = burn_model(INPUT)
-    if(MODEL == 'cut'):
-        results = cut_model(INPUT)
-    return results
 
 # load model
 burn_model = YOLO('./weights/SkinBurn.pt')
-abrasion_model = YOLO('./weights/SkinBurn.pt')
-cut_model = YOLO('./weights/SkinBurn.pt')
+abrasion_model = YOLO('./weights/abrasion.pt')
+cut_model = YOLO('./weights/Cut.pt')
+
+test = 'https://www.collinsdictionary.com/images/full/hand_large_702121612_1000.jpg'
+
+#test
+def predict(INPUT, MODEL):
+    if(MODEL == 'abrasion'):
+        results = abrasion_model(INPUT)
+    elif(MODEL == 'burn'):
+        results = burn_model(INPUT)
+    elif(MODEL == 'cut'):
+        results = cut_model(INPUT)
+    return [float(results[0].probs.top1conf * 100), results[0].probs.top1 + 1]
